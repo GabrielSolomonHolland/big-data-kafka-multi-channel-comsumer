ce75c638ad01efd948ae80b93d473808a2567586
GabrielSolomonHolland <69911029+GabrielSolomonHolland@users.noreply.github.com>
2022-11-07 09:11:13 -0600

    add consumer

--- src/main/java/com/mycompany/app/Consumer.java
+++ src/main/java/com/mycompany/app/Consumer.java
@@ -8,6 +8,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,7 +27,30 @@ public class Consumer {
 */
     public static void main(String[] args)
     {
-        
+        Logger logger = LoggerFactory.getLogger(Consumer.class.getName());
+        String bootstrapServers = "localhost:9092";        
+
+        // create Consumer properties
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "dreloemisleadme");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+
+        //subscription (wow even open source projects are making you subscribe, disgusting)
+        consumer.subscribe(Arrays.asList("Rack1Temps"));
+
+        while(true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record: records)
+            {
+                System.out.println(record.topic() + ", " + record.key() + ", " + record.value());
+            }
+        }
+
     }
     
 }
